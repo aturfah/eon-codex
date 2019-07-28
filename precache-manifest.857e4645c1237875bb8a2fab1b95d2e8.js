59499e10761a4a0d1aea67db2e37c91f25fad71e
Ali Turfah <aturfah@gmail.com>
2019-07-28 16:47:56 -0400

    Updates

---
+++ precache-manifest.857e4645c1237875bb8a2fab1b95d2e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6fc8088324a91ab5cea8dd32823cdd",
+    "url": "/eox-codex/index.html"
+  },
+  {
+    "revision": "783a9d802fb69f0b8198",
+    "url": "/eox-codex/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/eox-codex/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "783a9d802fb69f0b8198",
+    "url": "/eox-codex/static/js/main.36c6392c.chunk.js"
+  },
+  {
+    "revision": "a3167f54472d5366467e",
+    "url": "/eox-codex/static/js/runtime~main.905525f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/eox-codex/static/media/logo.5d5d9eef.svg"
+  }
+]);

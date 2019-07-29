adf8284a08cac14622f4214fdf35b672785ddae5
Ali Turfah <aturfah@gmail.com>
2019-07-29 13:07:38 -0400

    Refactor how App is built, also pass in filters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import FilterPanel from './FilterPanel/FilterPanel';
 import Compendium from './Compendium/Compendium';
 
-function App() {
-  return (
-    <div className="App">
-      <FilterPanel></FilterPanel>
-      <Compendium></Compendium>
-    </div>
-  );
+
+class App extends Component {
+  /**
+   * Initializes App
+   * @param {Object} props Properties of object
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      filters: {}
+    };
+  }
+
+  updateFilters(newFilters) {
+    const newState = this.state;
+    newState.filters = newFilters;
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <FilterPanel updateFilters={this.updateFilters.bind(this)}></FilterPanel>
+        <Compendium filters={this.state.filters}></Compendium>
+      </div>
+    );  
+  }
 }
 
 export default App;

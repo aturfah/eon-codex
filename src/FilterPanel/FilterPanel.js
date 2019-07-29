b156f4046d3ea3c80d74e7196822896228cfa1ff
Ali Turfah <aturfah@gmail.com>
2019-07-29 14:08:02 -0400

    Add buttons to filter items/monsters

--- src/FilterPanel/FilterPanel.js
+++ src/FilterPanel/FilterPanel.js
@@ -4,6 +4,9 @@ import './FilterPanel.css';
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 /**
  * Dropdown element for class selection
@@ -17,7 +20,15 @@ class FilterPanel extends Component {
     return (
       <Container fluid={true} className="FilterPanel">
         <Row>
-          <Col xs="4">I am FilterPanel. Fear me.</Col>
+          <Col xs="4">
+            <p>I am FilterPanel. Fear me.</p>
+            <ButtonToolbar className="CenterToolbar">
+              <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+                <ToggleButton value={1}>Items</ToggleButton>
+                <ToggleButton value={2}>Monsters</ToggleButton>
+              </ToggleButtonGroup>
+            </ButtonToolbar>
+          </Col>
           <Col xs="4">I am FilterPanel. Hate me.</Col>
           <Col xs="4">I am FilterPanel. Love me.</Col>
         </Row>

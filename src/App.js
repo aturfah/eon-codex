3daab2c0ac43b48efcdb3a148222665274259deb
Ali Turfah <aturfah@gmail.com>
2019-07-30 14:14:56 -0400

    Parse out & pass down locations

--- src/App.js
+++ src/App.js
@@ -5,6 +5,38 @@ import Compendium from './Compendium/Compendium';
 import itemData from './ItemData';
 import monsterData from './MonsterData';
 
+function defaultState() {
+  return {
+    filters: {}
+  };
+}
+
+function getLocations() {
+  // Get first list of items
+  const raw_output = new Set();
+  itemData.forEach(function (item, index) {
+    raw_output.add(item.location.toLowerCase());
+  });
+
+  monsterData.forEach(function (item, index) {
+    raw_output.add(item.loc.toLowerCase());
+  });
+
+  // Remove all composite locations
+  const final_set = new Set();
+  raw_output.forEach(function (item, index) {
+    if(!item.includes('and')) {
+      final_set.add(item);
+    }
+  });
+
+  // Sort and return
+  const output = Array.from(final_set);
+  output.sort();
+  return output;
+}
+
+
 class App extends Component {
   /**
    * Initializes App
@@ -12,19 +44,13 @@ class App extends Component {
    */
   constructor(props) {
     super(props);
-    this.state = this.defaultState();
-  }
-
-  defaultState() {
-    return {
-      filters: {}
-    };
+    this.state = defaultState();
   }
 
   updateFilters(key, filterValue, clearFlag) {
     let newState = null;
     if (clearFlag === true) {
-      newState = this.defaultState()
+      newState = defaultState()
     }
     else {
       newState = this.state;
@@ -35,9 +61,12 @@ class App extends Component {
   }
 
   render() {
+    const locations = getLocations();
     return (
       <div className="App">
-        <FilterPanel updateFilters={this.updateFilters.bind(this)}>
+        <FilterPanel
+          updateFilters={this.updateFilters.bind(this)}
+          locations={locations}>
         </FilterPanel>
         <Compendium
           filters={this.state.filters}

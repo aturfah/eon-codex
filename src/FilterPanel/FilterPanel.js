5fb2dd3c254e7ab2a8cc8607f799553b75fb0ec0
Ali Turfah <aturfah@umich.edu>
2019-07-31 15:40:00 -0400

    Add logic for setting rare item flag

--- src/FilterPanel/FilterPanel.js
+++ src/FilterPanel/FilterPanel.js
@@ -22,8 +22,11 @@ class FilterPanel extends Component {
     super(props);
     this.itemFlag = true;
     this.activeLocation = this.props.locations[0];
+    this.rareItemFlag = '(All)';
+
     this._toggleItemFlag = this._toggleItemFlag.bind(this);
     this.locationDropdownSelect = this.locationDropdownSelect.bind(this);
+    this.rareItemDropdownSelect = this.rareItemDropdownSelect.bind(this);
     this.toggleItemFlagOn();
   }
 
@@ -60,7 +63,6 @@ class FilterPanel extends Component {
   }
 
   locationDropdownSelect(event) {
-    console.log("HI!!!!")
     const dropdown = this.refs.locationDropdownSelect;
     dropdown.title = event;
     this.activeLocation = event;
@@ -70,6 +72,34 @@ class FilterPanel extends Component {
         this.props.updateFilters('location', null);
     }
 
+  }
+
+  buildRareItemDropdown() {
+    const buttonList = [];
+    ['(All)', 'Yes', 'No'].forEach(function (val) {
+        buttonList.push(<Dropdown.Item eventKey={val}>{val}</Dropdown.Item>)
+    });
+
+    return <DropdownButton
+        size="sm"
+        id="rare-item-dropdown-select"
+        ref="rareItemDropdownSelect"
+        title={this.rareItemFlag}
+        onSelect={this.rareItemDropdownSelect}
+        >
+      {buttonList}
+    </DropdownButton>
+  }
+
+  rareItemDropdownSelect(event) {
+    const dropdown = this.refs.rareItemDropdownSelect;
+    dropdown.title = event;
+    this.rareItemFlag = event;
+    if (event !== '(All)') {
+        this.props.updateFilters('rareItem', event);
+    } else {
+        this.props.updateFilters('rareItem', null);
+    }
   }
 
   /**
@@ -92,6 +122,7 @@ class FilterPanel extends Component {
 
     // Build dropdown for Locations
     const locationDropdown = this.buildLocationDropdown();
+    const rareItemDropdown = this.buildRareItemDropdown();
 
     return (
       <Container fluid={true} className="FilterPanel">
@@ -118,6 +149,7 @@ class FilterPanel extends Component {
               </ToggleButtonGroup>
             </ButtonToolbar>
             {locationDropdown}
+            {rareItemDropdown}
           </Col>
           <Col xs="12" md="8" hidden={!itemFiltersVisible}>I am Items FilterPanel. Hate me.</Col>
           <Col xs="12" md="8" hidden={itemFiltersVisible}>I am Monsters FilterPanel. Love me.</Col>

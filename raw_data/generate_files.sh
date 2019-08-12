bf5aed661e817d20d30a01d6f01f393138ad8b31
Ali Turfah <aturfah@gmail.com>
2019-08-12 12:04:09 -0400

    Add messsages

--- raw_data/generate_files.sh
+++ raw_data/generate_files.sh
@@ -1,10 +1,12 @@
 python3 parser.py
 
 if test -f "ItemData.js"; then
+    echo "Moving Item Data File..."
     mv ItemData.js ../src/ItemData.js;
 fi
 
 if test -f "MonsterData.js"; then
+    echo "Moving Monster Data File..."
     mv MonsterData.js ../src/MonsterData.js;
 fi
 

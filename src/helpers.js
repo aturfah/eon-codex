09ee1589fe4e42a4aa9869cbd698e740f219f627
Ali Turfah <aturfah@umich.edu>
2019-08-30 14:02:26 -0400

    Formatting & Vulnerability buttons

--- src/helpers.js
+++ src/helpers.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 function capitalizeFirst(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export function capitalizeWords(inputStr) {
-    const outputArr = [];
-    inputStr.split(' ').forEach(function (word) {
-        outputArr.push(capitalizeFirst(word));
-    });
+  const outputArr = [];
+  inputStr.split(' ').forEach(function (word) {
+    outputArr.push(capitalizeFirst(word));
+  });
 
-    return outputArr.join(' ');
+  return outputArr.join(' ');
 }
 
 export function isNumber(input) {
-    const re = /^[0-9\b]*$/;
-    return re.test(input) || !input;
+  const re = /^[0-9\b]*$/;
+  return re.test(input) || !input;
 }
 
 /**
@@ -24,19 +27,52 @@ export function isNumber(input) {
  * @return {Object} Map from filename to URL
  */
 export function importAll(r) {
-    const images = {};
-    r.keys().forEach((item, index) => {
-        images[item.replace('./', '')] = r(item);
-    });
-    return images;
+  const images = {};
+  r.keys().forEach((item, index) => {
+    images[item.replace('./', '')] = r(item);
+  });
+  return images;
 }
 
 export function formatVulnerability(number) {
-    if (number < 100) {
-        return <span className="RedNumber">{number}%</span>
-    } else if (number > 100) {
-        return <span className="GreenNumber">{number}%</span>
-    } else {
-        return <span>{number}%</span>
-    }
+  if (number < 100) {
+    return <span className="RedNumber">{number}%</span>
+  } else if (number > 100) {
+    return <span className="GreenNumber">{number}%</span>
+  } else {
+    return <span>{number}%</span>
+  }
+}
+
+export function buildVulnerabilityButtons(btnName, toggleCallback, status) {
+  const btnGroupName = '<>VulnerabilityOpts'.replace(btnName);
+  console.log(status);
+
+  return <ButtonToolbar className="CenterToolbar paddedButton" >
+    <ToggleButtonGroup type="radio" size="sm" name="ItemMonstOpts" defaultValue={1}>
+      <ToggleButton
+        onClick={toggleCallback}
+        value={1}
+        //variant={itemButtonVariant}
+      >
+        Weak
+      </ToggleButton>
+      <ToggleButton
+        size="sm"
+        onClick={toggleCallback}
+        value={2}
+        //variant={monsterButtonVariant}
+      >
+        Neutral
+      </ToggleButton>
+      <ToggleButton
+        size="sm"
+        onClick={toggleCallback}
+        value={3}
+        //variant={monsterButtonVariant}
+      >
+        Resistant
+      </ToggleButton>
+    </ToggleButtonGroup>
+  </ButtonToolbar>
 }
